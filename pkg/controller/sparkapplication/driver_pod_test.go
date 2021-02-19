c5aa878d4ab263baad89d380a4c2bcdfa026ca2b
Michaela Wilkins <michaelawilkins@lyft.com>
2021-02-19 15:46:12 -0500

    adding test

---
+++ pkg/controller/sparkapplication/driver_pod_test.go
@@ -0,0 +1,67 @@
+package sparkapplication
+
+import (
+	"testing"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	kubeclientfake "k8s.io/client-go/kubernetes/fake"
+
+	"github.com/GoogleCloudPlatform/spark-on-k8s-operator/pkg/apis/sparkoperator.k8s.io/v1beta2"
+	"github.com/stretchr/testify/assert"
+)
+
+func newFakePodManager(pods ...*corev1.Pod) clientSubmissionPodManager {
+	kubeClient := kubeclientfake.NewSimpleClientset()
+	informerFactory := informers.NewSharedInformerFactory(kubeClient, 0*time.Second)
+	podInformer := informerFactory.Core().V1().Pods()
+	lister := podInformer.Lister()
+	for _, pod := range pods {
+		if pod != nil {
+			podInformer.Informer().GetIndexer().Add(pod)
+			kubeClient.CoreV1().Pods(pod.GetNamespace()).Create(pod)
+		}
+	}
+	return &realClientSubmissionPodManager{
+		podLister:  lister,
+		kubeClient: kubeClient,
+	}
+}
+
+func TestCreateDriverPod(t *testing.T) {
+
+	// Case 1: Image doesn't exist.
+	app := &v1beta2.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Status: v1beta2.SparkApplicationStatus{},
+	}
+
+	podManager := newFakePodManager(nil)
+	submissionID, driverPodName, err := podManager.createClientDriverPod(app)
+	assert.NotNil(t, err)
+	assert.Empty(t, submissionID)
+	assert.Empty(t, driverPodName)
+
+	// Case 2:  Driver Pod creation successful.
+	app = &v1beta2.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Spec: v1beta2.SparkApplicationSpec{
+			Image: stringptr("spark-base-image"),
+		},
+		Status: v1beta2.SparkApplicationStatus{},
+	}
+	podManager = newFakePodManager(nil)
+	submissionID, driverPodName, err = podManager.createClientDriverPod(app)
+	assert.Nil(t, err)
+	assert.NotNil(t, submissionID)
+	assert.NotNil(t, driverPodName)
+
+}
